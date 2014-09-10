1f7c4e348162148c4661935d02089a067ffeb3da
Cyrus Huang <cyrushuang@wl-200-242.wpa.claremont.edu>
2014-09-09 23:40:03 -0700

    Initial Commit

---
+++ ViewTest/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  ViewTest
+//
+//  Created by Cyrus Huang on 9/9/14.
+//  Copyright (c) 2014 Cyrus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
